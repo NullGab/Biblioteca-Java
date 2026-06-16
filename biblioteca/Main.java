d2a7ce606f45f4e2bab4b405288b939b2960c0c8
Gabriel <gabriel.c.silva2@outlook.com>
2026-06-15 23:44:47 -0300

    Removendo arquivos antigos

--- biblioteca/Main.java
+++ biblioteca/Main.java
@@ -1,22 +1,26 @@
 package biblioteca;
 
 import biblioteca.adaptador.entrada.swing.BibliotecaFrame;
+import biblioteca.adaptador.saida.persistencia.EmprestimoRepositoryAdp;
 import biblioteca.adaptador.saida.persistencia.LivroRepositoryAdp;
+import biblioteca.aplicacao.EmprestimoRepository;
+import biblioteca.aplicacao.EmprestimoService;
 import biblioteca.aplicacao.LivroRepository;
 import biblioteca.aplicacao.LivroService;
 
 public class Main {
   public static void main(String[] args) {
-
     System.out.println("LOG: Inicializando sistema...");
 
-    LivroRepository repository = new LivroRepositoryAdp();
+    LivroRepository livroRepository = new LivroRepositoryAdp();
+    EmprestimoRepository emprestimoRepository = new EmprestimoRepositoryAdp();
 
-    LivroService service = new LivroService(repository);
+    LivroService livroService = new LivroService(livroRepository);
+    EmprestimoService emprestimoService = new EmprestimoService(emprestimoRepository, livroRepository);
 
-    BibliotecaFrame frame = new BibliotecaFrame(service);
+    BibliotecaFrame frame = new BibliotecaFrame(livroService, emprestimoService);
 
     System.out.println("LOG: Grafo montado. Abrindo interface gráfica.");
     frame.exibir();
   }
-} 
+}
